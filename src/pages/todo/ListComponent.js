6e94dcd9bd7190942f08f97e876367c89f064ba9
SuanChoi-Claire <clairechoi.h@gmail.com>
2025-04-14 12:25:21 +0900

    p148 ListComponent에서 출력하기

--- src/pages/todo/ListComponent.js
+++ src/pages/todo/ListComponent.js
@@ -27,7 +27,33 @@ const ListComponent = () => {
     });
   }, [page, size]);
 
-  return <div>Todo List Component</div>;
+  return (
+    <div>
+      Todo List Component
+      <div className="border-2 border-blue-100 mt-10 mr-2 ml-2">
+        <div className="flex flex-wrap mx-auto justify-center p-6">
+          {serverData.dtoList.map((todo) => (
+            <div
+              key={todo.tno}
+              className="w-full min-w-[400px] p-2 m-2 rounded shadow-md"
+            >
+              <div className="flex">
+                <div className="font-extrabold text-1xl p-2 w-1/12">
+                  {todo.tno}
+                </div>
+                <div className="text-1xl m-1 p-2 w-8/12 font-extrabold">
+                  {todo.title}
+                </div>
+                <div className="text-1xl m-1 p-2 w-2/10 font-medium">
+                  {todo.dueDate}
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ListComponent;
